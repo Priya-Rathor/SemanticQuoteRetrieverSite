3e99b7db9c2c2d49e02149a1cb8bd677058dc236
Priya Rathor <rathorpriya1718@gmail.com>
2025-07-06 21:14:13 +0530

    first commit

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ retriever, df = load_retriever()
 
 
 
+
+
 if query:
     with st.spinner("Retrieving relevant quotes and generating answer..."):
         context_quotes = retriever.retrieve(query)
